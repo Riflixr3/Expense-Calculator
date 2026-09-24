23bc5e9ae26b2bcde0f927e9e96077cad62ce4e1
Riflixr <shamsujjuharifat@gmail.com>
2026-09-24 22:16:19 +0600

    Initial Commit

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
+import sys
+
 def calculate(total, people, tip):
+
     if tip > 0:
         tip_amount = total * (tip / 100)
         total_with_tip = total + tip_amount
         pay = round(total_with_tip / people, 2)
 
-        print("Each person pays:", pay)
+
+        print("Bill:",total)
+        print("Tip: ",tip_amount)
+        print("Total: ",total_with_tip)
+        print("Per person: ",pay)
 
     else:
         pay = round(total / people, 2)
 
-        print("Each person pays:", pay)
+        print("Bill:",total)
+        print("Tip: 0")
+        print("Total: ",total)
+        print("Per person: ",pay)
 
 
 total = float(input("Enter the total bill: "))
+
+if total <= 0:
+    print("Invalid bill")
+    sys.exit()
+    
+        
+
 people = int(input("Enter the total number of people: "))
+
+if people <= 0:
+        print("People can't be ", people)
+        sys.exit()
+
+ 
 tip = float(input("How much % tip is: "))
 
-calculate(total, people, tip)
+if tip <= 0:
+    tip = 0 
+
+calculate(total, people,tip)
+
